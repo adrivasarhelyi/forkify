3bf123bc9eb587e2fcebaf1a57bb7f32329080d5
Adri Vasarhelyi <adri.palasti.number1@gmail.com>
2024-08-01 07:12:47 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'cbe722b9-46fa-447e-bd14-dba4d2542d80';
+export const MODAL_CLOSE_SEC = 2.5;
